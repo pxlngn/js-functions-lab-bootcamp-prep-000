12ca2e73c6b79c2411c44fbaf7fb98308d232c80
jthomps86@gmail.com <jthomps86@gmail.com>
2018-07-26 15:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function happyHolidayCountdown(days, holiday) {
+function happyHolidayCountdown(holiday, days) {
   return `It's ${days} until ${holiday}!`
 }
